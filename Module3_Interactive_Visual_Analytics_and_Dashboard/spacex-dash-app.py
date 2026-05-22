15948070b7cc7a7ca40f9fb2de23137b68c2fa5d
PhuongNTM195 <phuongntm195@gmail.com>
2026-05-22 21:52:46 +0700

    create module3 Interactive Visual Analytics and Dashboard

--- Module3_Interactive_Visual_Analytics_and_Dashboard/spacex-dash-app.py
+++ Module3_Interactive_Visual_Analytics_and_Dashboard/spacex-dash-app.py
@@ -1,12 +1,10 @@
 # Import required libraries
 import pandas as pd
 import dash
-import dash_html_components as html
-import dash_core_components as dcc
-from dash.dependencies import Input, Output, State
-import plotly.graph_objects as go
+from dash import html
+from dash import dcc
+from dash.dependencies import Input, Output
 import plotly.express as px
-from dash import no_update
 
 # Read the airline data into pandas dataframe
 spacex_df = pd.read_csv("spacex_launch_dash.csv")
@@ -19,13 +17,17 @@ app = dash.Dash(__name__)
 # Create an app layout
 launch_sites = []
 launch_sites.append({'label': 'All Sites', 'value': 'All Sites'})
-for item in spacex_df["Launch Site"].value_counts().index:
-    launch_sites.append({'label': item, 'value': item})
+launch_sites.append({'label': 'CCAFS LC-40', 'value': 'CCAFS LC-40'})
+launch_sites.append({'label': 'VAFB SLC-4E', 'value': 'VAFB SLC-4E'})
+launch_sites.append({'label': 'KSC LC-39A', 'value': 'KSC LC-39A'})
+launch_sites.append({'label': 'CCAFS SLC-40', 'value': 'CCAFS SLC-40'})
+
 app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
                                         style={'textAlign': 'center', 'color': '#503D36',
                                                'font-size': 40}),
                                 # TASK 1: Add a dropdown list to enable Launch Site selection
                                 # The default select value is for ALL sites
+                                # dcc.Dropdown(id='site-dropdown',...)
                                 dcc.Dropdown(id='site-dropdown', options = launch_sites, value = 'All Sites', placeholder = "Select a Launch Site here", searchable = True),
                                 html.Br(),
 
@@ -36,6 +38,7 @@ app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
 
                                 html.P("Payload range (Kg):"),
                                 # TASK 3: Add a slider to select payload range
+                                #dcc.RangeSlider(id='payload-slider',...)
                                 dcc.RangeSlider(id='payload-slider', min = 0, max = 10000, step = 1000, value = [min_payload, max_payload], marks={ 2500: {'label': '2500 (Kg)'}, 5000: {'label': '5000 (Kg)'}, 7500: {'label': '7500 (Kg)'}}),
 
                                 # TASK 4: Add a scatter chart to show the correlation between payload and launch success
@@ -44,19 +47,47 @@ app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
 
 # TASK 2:
 # Add a callback function for `site-dropdown` as input, `success-pie-chart` as output
-@app.callback( Output(component_id='success-pie-chart', component_property='figure'),
-               Input(component_id='site-dropdown', component_property='value')
+@app.callback(
+    Output(component_id='success-pie-chart', component_property='figure'),
+    Input(component_id='site-dropdown', component_property='value')
 )
-# Add computation to callback function and return graph
+
 def select(inputt):
+
     if inputt == 'All Sites':
-        new_df = spacex_df.groupby(['Launch Site'])["class"].sum().to_frame()
-        new_df = new_df.reset_index()
-        fig = px.pie(new_df, values='class', names='Launch Site', title='Total Success Launches by Site')
+
+        new_df = spacex_df.groupby('Launch Site')['class'].sum().reset_index()
+
+        fig = px.pie(
+            new_df,
+            values='class',
+            names='Launch Site',
+            title='Total Success Launches by Site'
+        )
+
     else:
-        new_df = spacex_df[spacex_df["Launch Site"] == inputt]["class"].value_counts().to_frame()
-        new_df["name"] = ["Failure", "Success"]
-        fig = px.pie(new_df, values='class', names='name', title='Total Success Launches for ' + inputt)
+
+        new_df = (
+            spacex_df[spacex_df["Launch Site"] == inputt]
+            ['class']
+            .value_counts()
+            .reset_index()
+        )
+
+        new_df.columns = ['class', 'count']
+
+        new_df['Result'] = new_df['class'].map({
+            0: 'Failure',
+            1: 'Success'
+        })
+
+        fig = px.pie(
+            new_df,
+            values='count',
+            names='Result',
+            title='Total Success Launches for ' + inputt
+        )
+
     return fig
 
 # TASK 4:
@@ -80,7 +111,6 @@ def scatter(input1, input2):
         fig2 = px.scatter(new_df3, y="class", x="Payload Mass (kg)", color="Booster Version Category")
     return fig2
 
-
 # Run the app
 if __name__ == '__main__':
-    app.run_server()
+    app.run()
